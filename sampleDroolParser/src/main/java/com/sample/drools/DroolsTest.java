bec8734dd91d8fef2f1e22f4b413a462e3fc5670
Pradeepkumar <cgpradeepkumar@gmail.com>
2018-04-18 08:33:02 +0530

    initial

---
+++ sampleDroolParser/src/main/java/com/sample/drools/DroolsTest.java
@@ -0,0 +1,39 @@
+package com.sample.drools;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+import org.drools.compiler.compiler.DroolsParserException;
+import org.drools.compiler.compiler.PackageBuilder;
+import org.drools.core.RuleBase;
+import org.drools.core.RuleBaseFactory;
+import org.drools.core.WorkingMemory;
+
+
+public class DroolsTest {
+	public static void main(String[] args) throws DroolsParserException,
+			IOException {
+		DroolsTest droolsTest = new DroolsTest();
+		droolsTest.executeDrools();
+	}
+
+	public void executeDrools() throws DroolsParserException, IOException {
+		PackageBuilder packageBuilder = new PackageBuilder();
+		String ruleFile = "/com/rules/droolsRule.drl";
+		InputStream resourceAsStream = getClass().getResourceAsStream(ruleFile);
+		Reader reader = new InputStreamReader(resourceAsStream);
+		packageBuilder.addPackageFromDrl(reader);
+		org.drools.core.rule.Package rulesPackage = packageBuilder.getPackage();
+		RuleBase ruleBase = RuleBaseFactory.newRuleBase();
+		ruleBase.addPackage(rulesPackage);
+		WorkingMemory workingMemory = ruleBase.newStatefulSession();
+		Person person = new Person();
+		person.setName("Shamik Mitra");
+		person.setTime(7);
+		workingMemory.insert(person);
+		workingMemory.fireAllRules();
+		System.out.println(person.getGreet());
+	}
+}
